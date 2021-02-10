5ef63d00b161210a67ddf63784b741f4a795e370
Scott <sra405@gmail.com>
2021-02-10 23:38:24 +0000

    All pieces defined (initially at least)

--- Chess/pieces.py
+++ Chess/pieces.py
@@ -21,10 +21,18 @@ class AllPieces:
         self.pieces_def_loc = os.path.join(pieces_def_loc, pieces_def_filename)
         # load up the definitions
         self._load_pieces_defs()
+        # figure out potential move deltas
+        self._turn_moves_to_deltas()
 
     def _load_pieces_defs(self):
         with open(self.pieces_def_loc) as f:
             self.pieces_defs = json.load(f)
+    
+    def _turn_moves_to_deltas(self):
+        for piece, defs in self.pieces_defs.items():
+            if all([isinstance(move, (list, tuple)) for move in defs["moves"]):
+                self.pieces_defs[piece]["deltas"] = defs["moves"]
+        print(self.pieces_defs)
 
 if __name__ == "__main__":
     allPieces = AllPieces()

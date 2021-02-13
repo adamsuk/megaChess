f8284bb7f3d0f0ac7a07955d06ced859132aeb20
Scott Adams <sra405@hotmail.co.uk>
2021-02-13 20:09:57 +0000

    general pygame structure

--- Chess/board.py
+++ Chess/board.py
@@ -1,43 +1,8 @@
-"""
-checkers.py
+import pygame
+import sys
+from pygame import locals
 
-A simple checkers engine written in Python with the pygame 1.9.1 libraries.
-
-Here are the rules I am using: http://boardgames.about.com/cs/checkersdraughts/ht/play_checkers.htm
-
-I adapted some code from checkers.py found at 
-http://itgirl.dreamhosters.com/itgirlgames/games/Program%20Leaders/ClareR/Checkers/checkers.py starting on line 159 of my program.
-
-This is the final version of my checkers project for Programming Workshop at Marlboro College. The entire thing has been rafactored and made almost completely object oriented.
-
-Funcitonalities include:
-
-- Having the pieces and board drawn to the screen
-
-- The ability to move pieces by clicking on the piece you want to move, then clicking on the square you would
-  like to move to. You can change you mind about the piece you would like to move, just click on a new piece of yours.
-
-- Knowledge of what moves are legal. When moving pieces, you'll be limited to legal moves.
-
-- Capturing
-
-- DOUBLE capturing etc.
-
-- Legal move and captive piece highlighting
-
-- Turn changes
-
-- Automatic kinging and the ability for them to move backwords
-
-- Automatic check for and end game. 
-
-- A silky smoooth 60 FPS!
-
-Everest Witman - May 2014 - Marlboro College - Programming Workshop 
-"""
-
-import pygame, sys
-from pygame.locals import *
+from common import Colours, Directions
 
 try:
     # Python 2
@@ -47,227 +12,6 @@ except NameError:
     xrange = range
 
 pygame.font.init()
-
-##COLORS##
-#             R    G    B 
-WHITE    = (255, 255, 255)
-BLUE     = (  0,   0, 255)
-RED      = (255,   0,   0)
-BLACK    = (  0,   0,   0)
-GOLD     = (255, 215,   0)
-HIGH     = (160, 190, 255)
-
-##DIRECTIONS##
-NORTHWEST = "northwest"
-NORTHEAST = "northeast"
-SOUTHWEST = "southwest"
-SOUTHEAST = "southeast"
-
-class Game:
-	"""
-	The main game control.
-	"""
-
-	def __init__(self):
-		self.graphics = Graphics()
-		self.board = Board()
-		
-		self.turn = BLUE
-		self.selected_piece = None # a board location. 
-		self.hop = False
-		self.selected_legal_moves = []
-
-	def setup(self):
-		"""Draws the window and board at the beginning of the game"""
-		self.graphics.setup_window()
-
-	def event_loop(self):
-		"""
-		The event loop. This is where events are triggered 
-		(like a mouse click) and then effect the game state.
-		"""
-		self.mouse_pos = self.graphics.board_coords(pygame.mouse.get_pos()) # what square is the mouse in?
-		if self.selected_piece != None:
-			self.selected_legal_moves = self.board.legal_moves(self.selected_piece, self.hop)
-
-		for event in pygame.event.get():
-
-			if event.type == QUIT:
-				self.terminate_game()
-
-			if event.type == MOUSEBUTTONDOWN:
-				if self.hop == False:
-					if self.board.location(self.mouse_pos).occupant != None and self.board.location(self.mouse_pos).occupant.color == self.turn:
-						self.selected_piece = self.mouse_pos
-
-					elif self.selected_piece != None and self.mouse_pos in self.board.legal_moves(self.selected_piece):
-
-						self.board.move_piece(self.selected_piece, self.mouse_pos)
-					
-						if self.mouse_pos not in self.board.adjacent(self.selected_piece):
-							self.board.remove_piece((self.selected_piece[0] + (self.mouse_pos[0] - self.selected_piece[0]) / 2, self.selected_piece[1] + (self.mouse_pos[1] - self.selected_piece[1]) / 2))
-						
-							self.hop = True
-							self.selected_piece = self.mouse_pos
-
-						else:
-							self.end_turn()
-
-				if self.hop == True:					
-					if self.selected_piece != None and self.mouse_pos in self.board.legal_moves(self.selected_piece, self.hop):
-						self.board.move_piece(self.selected_piece, self.mouse_pos)
-						self.board.remove_piece((self.selected_piece[0] + (self.mouse_pos[0] - self.selected_piece[0]) / 2, self.selected_piece[1] + (self.mouse_pos[1] - self.selected_piece[1]) / 2))
-
-					if self.board.legal_moves(self.mouse_pos, self.hop) == []:
-							self.end_turn()
-
-					else:
-						self.selected_piece = self.mouse_pos
-
-
-	def update(self):
-		"""Calls on the graphics class to update the game display."""
-		self.graphics.update_display(self.board, self.selected_legal_moves, self.selected_piece)
-
-	def terminate_game(self):
-		"""Quits the program and ends the game."""
-		pygame.quit()
-		sys.exit
-
-	def main(self):
-		""""This executes the game and controls its flow."""
-		self.setup()
-
-		while True: # main game loop
-			self.event_loop()
-			self.update()
-
-	def end_turn(self):
-		"""
-		End the turn. Switches the current player. 
-		end_turn() also checks for and game and resets a lot of class attributes.
-		"""
-		if self.turn == BLUE:
-			self.turn = RED
-		else:
-			self.turn = BLUE
-
-		self.selected_piece = None
-		self.selected_legal_moves = []
-		self.hop = False
-
-		if self.check_for_endgame():
-			if self.turn == BLUE:
-				self.graphics.draw_message("RED WINS!")
-			else:
-				self.graphics.draw_message("BLUE WINS!")
-
-	def check_for_endgame(self):
-		"""
-		Checks to see if a player has run out of moves or pieces. If so, then return True. Else return False.
-		"""
-		for x in xrange(8):
-			for y in xrange(8):
-				if self.board.location((x,y)).color == BLACK and self.board.location((x,y)).occupant != None and self.board.location((x,y)).occupant.color == self.turn:
-					if self.board.legal_moves((x,y)) != []:
-						return False
-
-		return True
-
-class Graphics:
-	def __init__(self):
-		self.caption = "Checkers"
-
-		self.fps = 60
-		self.clock = pygame.time.Clock()
-
-		self.window_size = 600
-		self.screen = pygame.display.set_mode((self.window_size, self.window_size))
-		self.background = pygame.image.load('resources/board.png')
-
-		self.square_size = self.window_size / 8
-		self.piece_size = self.square_size / 2
-
-		self.message = False
-
-	def setup_window(self):
-		"""
-		This initializes the window and sets the caption at the top.
-		"""
-		pygame.init()
-		pygame.display.set_caption(self.caption)
-
-	def update_display(self, board, legal_moves, selected_piece):
-		"""
-		This updates the current display.
-		"""
-		self.screen.blit(self.background, (0,0))
-		
-		self.highlight_squares(legal_moves, selected_piece)
-		self.draw_board_pieces(board)
-
-		if self.message:
-			self.screen.blit(self.text_surface_obj, self.text_rect_obj)
-
-		pygame.display.update()
-		self.clock.tick(self.fps)
-
-	def draw_board_squares(self, board):
-		"""
-		Takes a board object and draws all of its squares to the display
-		"""
-		for x in xrange(8):
-			for y in xrange(8):
-				pygame.draw.rect(self.screen, board[int(x)][int(y)].color, (x * self.square_size, y * self.square_size, self.square_size, self.square_size), )
-	
-	def draw_board_pieces(self, board):
-		"""
-		Takes a board object and draws all of its pieces to the display
-		"""
-		for x in xrange(8):
-			for y in xrange(8):
-				if board.matrix[int(x)][int(y)].occupant != None:
-					print(self.screen, board.matrix[int(x)][int(y)].occupant.color, self.pixel_coords((x,y)), self.piece_size)
-					pygame.draw.circle(self.screen, board.matrix[int(x)][int(y)].occupant.color, self.pixel_coords((x,y)), self.piece_size) 
-
-					if board.location((x,y)).occupant.king == True:
-						pygame.draw.circle(self.screen, GOLD, self.pixel_coords((x,y)), int (self.piece_size / 1.7), self.piece_size / 4)
-
-
-	def pixel_coords(self, board_coords):
-		"""
-		Takes in a tuple of board coordinates (x,y) 
-		and returns the pixel coordinates of the center of the square at that location.
-		"""
-		return (board_coords[0] * self.square_size + self.piece_size, board_coords[1] * self.square_size + self.piece_size)
-
-	def board_coords(self, pixel_coords_tuple):
-		"""
-		Does the reverse of pixel_coords(). Takes in a tuple of of pixel coordinates and returns what square they are in.
-		"""
-		pixel_x, pixel_y = pixel_coords_tuple
-		return (pixel_x / self.square_size, pixel_y / self.square_size)	
-
-	def highlight_squares(self, squares, origin):
-		"""
-		Squares is a list of board coordinates. 
-		highlight_squares highlights them.
-		"""
-		for square in squares:
-			pygame.draw.rect(self.screen, HIGH, (square[0] * self.square_size, square[1] * self.square_size, self.square_size, self.square_size))	
-
-		if origin != None:
-			pygame.draw.rect(self.screen, HIGH, (origin[0] * self.square_size, origin[1] * self.square_size, self.square_size, self.square_size))
-
-	def draw_message(self, message):
-		"""
-		Draws message to the screen. 
-		"""
-		self.message = True
-		self.font_obj = pygame.font.Font('freesansbold.ttf', 44)
-		self.text_surface_obj = self.font_obj.render(message, True, HIGH, BLACK)
-		self.text_rect_obj = self.text_surface_obj.get_rect()
-		self.text_rect_obj.center = (self.window_size / 2, self.window_size / 2)
 
 class Board:
 	def __init__(self):
@@ -287,23 +31,23 @@ class Board:
 		for x in xrange(8):
 			for y in xrange(8):
 				if (x % 2 != 0) and (y % 2 == 0):
-					matrix[int(y)][int(x)] = Square(WHITE)
+					matrix[int(y)][int(x)] = Square(Colours.WHITE)
 				elif (x % 2 != 0) and (y % 2 != 0):
-					matrix[int(y)][int(x)] = Square(BLACK)
+					matrix[int(y)][int(x)] = Square(Colours.BLACK)
 				elif (x % 2 == 0) and (y % 2 != 0):
-					matrix[int(y)][int(x)] = Square(WHITE)
+					matrix[int(y)][int(x)] = Square(Colours.WHITE)
 				elif (x % 2 == 0) and (y % 2 == 0): 
-					matrix[int(y)][int(x)] = Square(BLACK)
+					matrix[int(y)][int(x)] = Square(Colours.BLACK)
 
 		# initialize the pieces and put them in the appropriate squares
 
 		for x in xrange(8):
 			for y in xrange(3):
-				if matrix[int(x)][int(y)].color == BLACK:
-					matrix[int(x)][int(y)].occupant = Piece(RED)
+				if matrix[int(x)][int(y)].color == Colours.BLACK:
+					matrix[int(x)][int(y)].occupant = Piece(Colours.RED)
 			for y in xrange(5, 8):
-				if matrix[int(x)][int(y)].color == BLACK:
-					matrix[int(x)][int(y)].occupant = Piece(BLUE)
+				if matrix[int(x)][int(y)].color == Colours.BLACK:
+					matrix[int(x)][int(y)].occupant = Piece(Colours.BLUE)
 
 		return matrix
 
@@ -316,7 +60,7 @@ class Board:
 
 		for x in xrange(8):
 			for y in xrange(8):
-				if board[int(x)][int(y)].color == WHITE:
+				if board[int(x)][int(y)].color == Colours.WHITE:
 					board_string[int(x)][int(y)] = "WHITE"
 				else:
 					board_string[int(x)][int(y)] = "BLACK"
@@ -332,26 +76,26 @@ class Board:
 
 		>>> board = Board()
 
-		>>> board.rel(NORTHWEST, (1,2))
+		>>> board.rel(Directions.NORTHWEST, (1,2))
 		(0,1)
 
-		>>> board.rel(SOUTHEAST, (3,4))
+		>>> board.rel(Directions.SOUTHEAST, (3,4))
 		(4,5)
 
-		>>> board.rel(NORTHEAST, (3,6))
+		>>> board.rel(Directions.NORTHEAST, (3,6))
 		(4,5)
 
-		>>> board.rel(SOUTHWEST, (2,5))
+		>>> board.rel(Directions.SOUTHWEST, (2,5))
 		(1,6)
 		"""
 		x, y = coord_tuple
-		if dir == NORTHWEST:
+		if dir == Directions.NORTHWEST:
 			return (x - 1, y - 1)
-		elif dir == NORTHEAST:
+		elif dir == Directions.NORTHEAST:
 			return (x + 1, y - 1)
-		elif dir == SOUTHWEST:
+		elif dir == Directions.SOUTHWEST:
 			return (x - 1, y + 1)
-		elif dir == SOUTHEAST:
+		elif dir == Directions.SOUTHEAST:
 			return (x + 1, y + 1)
 		else:
 			return 0
@@ -361,7 +105,7 @@ class Board:
 		Returns a list of squares locations that are adjacent (on a diagonal) to (x,y).
 		"""
 		x, y = coord_tuple
-		return [self.rel(NORTHWEST, (x,y)), self.rel(NORTHEAST, (x,y)),self.rel(SOUTHWEST, (x,y)),self.rel(SOUTHEAST, (x,y))]
+		return [self.rel(Directions.NORTHWEST, (x,y)), self.rel(Directions.NORTHEAST, (x,y)),self.rel(Directions.SOUTHWEST, (x,y)),self.rel(Directions.SOUTHEAST, (x,y))]
 
 	def location(self, coord_tuple):
 		"""
@@ -379,14 +123,14 @@ class Board:
 		x, y = coord_tuple
 		if self.matrix[int(x)][int(y)].occupant != None:
 			
-			if self.matrix[int(x)][int(y)].occupant.king == False and self.matrix[int(x)][int(y)].occupant.color == BLUE:
-				blind_legal_moves = [self.rel(NORTHWEST, (x,y)), self.rel(NORTHEAST, (x,y))]
+			if self.matrix[int(x)][int(y)].occupant.king == False and self.matrix[int(x)][int(y)].occupant.color == Colours.BLUE:
+				blind_legal_moves = [self.rel(Directions.NORTHWEST, (x,y)), self.rel(Directions.NORTHEAST, (x,y))]
 				
-			elif self.matrix[int(x)][int(y)].occupant.king == False and self.matrix[int(x)][int(y)].occupant.color == RED:
-				blind_legal_moves = [self.rel(SOUTHWEST, (x,y)), self.rel(SOUTHEAST, (x,y))]
+			elif self.matrix[int(x)][int(y)].occupant.king == False and self.matrix[int(x)][int(y)].occupant.color == Colours.RED:
+				blind_legal_moves = [self.rel(Directions.SOUTHWEST, (x,y)), self.rel(Directions.SOUTHEAST, (x,y))]
 
 			else:
-				blind_legal_moves = [self.rel(NORTHWEST, (x,y)), self.rel(NORTHEAST, (x,y)), self.rel(SOUTHWEST, (x,y)), self.rel(SOUTHEAST, (x,y))]
+				blind_legal_moves = [self.rel(Directions.NORTHWEST, (x,y)), self.rel(Directions.NORTHEAST, (x,y)), self.rel(Directions.SOUTHWEST, (x,y)), self.rel(Directions.SOUTHEAST, (x,y))]
 
 		else:
 			blind_legal_moves = []
@@ -399,6 +143,8 @@ class Board:
 		If that location is empty, then legal_moves() returns an empty list.
 		"""
 		x, y = coord_tuple
+		# TODO some logic that ensures the legal positions are piece centered (currently passing in cursor centered but
+		#  this should also include logic to piece center coordinates.
 		blind_legal_moves = self.blind_legal_moves((x,y)) 
 		legal_moves = []
 
@@ -493,7 +239,7 @@ class Board:
 		"""
 		x, y = coord_tuple
 		if self.location((x,y)).occupant != None:
-			if (self.location((x,y)).occupant.color == BLUE and y == 0) or (self.location((x,y)).occupant.color == RED and y == 7):
+			if (self.location((x,y)).occupant.color == Colours.BLUE and y == 0) or (self.location((x,y)).occupant.color == Colours.RED and y == 7):
 				self.location((x,y)).occupant.king = True 
 
 class Piece:
@@ -505,10 +251,3 @@ class Square:
 	def __init__(self, color, occupant = None):
 		self.color = color # color is either BLACK or WHITE
 		self.occupant = occupant # occupant is a Square object
-
-def main():
-	game = Game()
-	game.main()
-
-if __name__ == "__main__":
-	main()
---
+++ Chess/game.py
@@ -0,0 +1,244 @@
+"""
+The main game control.
+"""
+import pygame
+import sys
+from pygame import locals
+
+from board import Board, Piece, Square
+from common import Colours, Directions
+
+try:
+    # Python 2
+    xrange
+except NameError:
+    # Python 3, xrange is now named range
+    xrange = range
+
+class Game:
+
+    def __init__(self):
+        """
+        The main game control.
+        """
+        self.graphics = Graphics()
+        self.board = Board()
+
+        self.turn = Colours.BLUE
+        self.selected_piece = None  # a board location.
+        self.hop = False
+        self.selected_legal_moves = []
+
+    def setup(self):
+        """Draws the window and board at the beginning of the game"""
+        self.graphics.setup_window()
+
+    def event_loop(self):
+        """
+        The event loop. This is where events are triggered
+        (like a mouse click) and then effect the game state.
+        """
+        self.mouse_pos = self.graphics.board_coords(pygame.mouse.get_pos())  # what square is the mouse in?
+        if self.selected_piece != None:
+            self.selected_legal_moves = self.board.legal_moves(self.selected_piece, self.hop)
+
+        for event in pygame.event.get():
+
+            if event.type == locals.QUIT:
+                self.terminate_game()
+
+            if event.type == locals.MOUSEBUTTONDOWN:
+                if self.hop == False:
+                    if self.board.location(self.mouse_pos).occupant != None and self.board.location(
+                            self.mouse_pos).occupant.color == self.turn:
+                        # TODO: this centers on the cursor not the piece
+                        self.selected_piece = self.mouse_pos
+
+                    elif self.selected_piece != None and self.mouse_pos in self.board.legal_moves(self.selected_piece):
+
+                        self.board.move_piece(self.selected_piece, self.mouse_pos)
+
+                        if self.mouse_pos not in self.board.adjacent(self.selected_piece):
+                            self.board.remove_piece((self.selected_piece[0] + (
+                                        self.mouse_pos[0] - self.selected_piece[0]) / 2, self.selected_piece[1] + (
+                                                                 self.mouse_pos[1] - self.selected_piece[1]) / 2))
+
+                            self.hop = True
+                            self.selected_piece = self.mouse_pos
+
+                        else:
+                            self.end_turn()
+
+                if self.hop == True:
+                    if self.selected_piece != None and self.mouse_pos in self.board.legal_moves(self.selected_piece,
+                                                                                                self.hop):
+                        self.board.move_piece(self.selected_piece, self.mouse_pos)
+                        self.board.remove_piece((self.selected_piece[0] + (
+                                    self.mouse_pos[0] - self.selected_piece[0]) / 2, self.selected_piece[1] + (
+                                                             self.mouse_pos[1] - self.selected_piece[1]) / 2))
+
+                    if self.board.legal_moves(self.mouse_pos, self.hop) == []:
+                        self.end_turn()
+
+                    else:
+                        self.selected_piece = self.mouse_pos
+
+    def update(self):
+        """Calls on the graphics class to update the game display."""
+        self.graphics.update_display(self.board, self.selected_legal_moves, self.selected_piece)
+
+    def terminate_game(self):
+        """Quits the program and ends the game."""
+        pygame.quit()
+        sys.exit
+
+    def main(self):
+        """"This executes the game and controls its flow."""
+        self.setup()
+
+        while True:  # main game loop
+            self.event_loop()
+            self.update()
+
+    def end_turn(self):
+        """
+        End the turn. Switches the current player.
+        end_turn() also checks for and game and resets a lot of class attributes.
+        """
+        if self.turn == Colours.BLUE:
+            self.turn = Colours.RED
+        else:
+            self.turn = Colours.BLUE
+
+        self.selected_piece = None
+        self.selected_legal_moves = []
+        self.hop = False
+
+        if self.check_for_endgame():
+            if self.turn == Colours.BLUE:
+                self.graphics.draw_message("RED WINS!")
+            else:
+                self.graphics.draw_message("BLUE WINS!")
+
+    def check_for_endgame(self):
+        """
+        Checks to see if a player has run out of moves or pieces. If so, then return True. Else return False.
+        """
+        for x in xrange(8):
+            for y in xrange(8):
+                if self.board.location((x, y)).color == Colours.BLACK and self.board.location(
+                        (x, y)).occupant != None and self.board.location((x, y)).occupant.color == self.turn:
+                    if self.board.legal_moves((x, y)) != []:
+                        return False
+
+        return True
+
+
+class Graphics:
+    def __init__(self):
+        self.caption = "megaChess"
+
+        self.fps = 60
+        self.clock = pygame.time.Clock()
+
+        self.window_size = 600
+        self.screen = pygame.display.set_mode((self.window_size, self.window_size))
+        # self.background = pygame.image.load('resources/board.png')
+
+        self.square_size = self.window_size / 8
+        self.piece_size = self.square_size / 2
+
+        self.message = False
+
+    def setup_window(self):
+        """
+        This initializes the window and sets the caption at the top.
+        """
+        pygame.init()
+        pygame.display.set_caption(self.caption)
+
+    def update_display(self, board, legal_moves, selected_piece):
+        """
+        This updates the current display.
+        """
+        # self.screen.blit(self.background, (0,0))
+
+        self.highlight_squares(legal_moves, selected_piece)
+        self.draw_board_pieces(board)
+
+        if self.message:
+            self.screen.blit(self.text_surface_obj, self.text_rect_obj)
+
+        pygame.display.update()
+        self.clock.tick(self.fps)
+
+    def draw_board_squares(self, board):
+        """
+        Takes a board object and draws all of its squares to the display
+        """
+        for x in xrange(8):
+            for y in xrange(8):
+                pygame.draw.rect(self.screen, board[int(x)][int(y)].color,
+                                 (x * self.square_size, y * self.square_size, self.square_size, self.square_size), )
+
+    def draw_board_pieces(self, board):
+        """
+        Takes a board object and draws all of its pieces to the display
+        """
+        for x in xrange(8):
+            for y in xrange(8):
+                if board.matrix[int(x)][int(y)].occupant != None:
+                    print(self.screen, board.matrix[int(x)][int(y)].occupant.color, self.pixel_coords((x, y)),
+                          self.piece_size)
+                    pygame.draw.circle(self.screen, board.matrix[int(x)][int(y)].occupant.color,
+                                       self.pixel_coords((x, y)), self.piece_size)
+
+                    if board.location((x, y)).occupant.king == True:
+                        pygame.draw.circle(self.screen, GOLD, self.pixel_coords((x, y)), int(self.piece_size / 1.7),
+                                           self.piece_size / 4)
+
+    def pixel_coords(self, board_coords):
+        """
+        Takes in a tuple of board coordinates (x,y)
+        and returns the pixel coordinates of the center of the square at that location.
+        """
+        return (
+        board_coords[0] * self.square_size + self.piece_size, board_coords[1] * self.square_size + self.piece_size)
+
+    def board_coords(self, pixel_coords_tuple):
+        """
+        Does the reverse of pixel_coords(). Takes in a tuple of of pixel coordinates and returns what square they are in.
+        """
+        pixel_x, pixel_y = pixel_coords_tuple
+        return (pixel_x / self.square_size, pixel_y / self.square_size)
+
+    def highlight_squares(self, squares, origin):
+        """
+        Squares is a list of board coordinates.
+        highlight_squares highlights them.
+        """
+        for square in squares:
+            pygame.draw.rect(self.screen, Colours.HIGH, (
+            square[0] * self.square_size, square[1] * self.square_size, self.square_size, self.square_size))
+
+        if origin != None:
+            pygame.draw.rect(self.screen, Colours.HIGH, (
+            origin[0] * self.square_size, origin[1] * self.square_size, self.square_size, self.square_size))
+
+    def draw_message(self, message):
+        """
+        Draws message to the screen.
+        """
+        self.message = True
+        self.font_obj = pygame.font.Font('freesansbold.ttf', 44)
+        self.text_surface_obj = self.font_obj.render(message, True, Colours.HIGH, Colours.BLACK)
+        self.text_rect_obj = self.text_surface_obj.get_rect()
+        self.text_rect_obj.center = (self.window_size / 2, self.window_size / 2)
+
+
+def main():
+	game = Game()
+	game.main()
+
+if __name__ == "__main__":
+	main()
